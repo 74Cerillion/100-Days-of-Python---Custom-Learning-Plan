7ff12427bec17fc027cae0163d5feb6154bc9886
74Cerillion <jonhotchner@gmail.com>
2026-08-28 16:35:39 -0400

    Added dashboard logic, input validation, and add product, add stock, and remove stock options

--- Day 12/main.py
+++ Day 12/main.py
@@ -1,2 +1,59 @@
 from product import Product
 
+def main():
+
+    products = {
+        '100': Product("Popcorn", "100", 2, 50),
+        '200': Product("Cookies", "200", 3, 100),
+    }
+
+    print(r"""
+    WELCOME TO INVENTORY MANAGEMENT! WHAT ACTION/S ARE WE PERFORMING TODAY?
+    """)
+
+    while True:
+        decision = input(r"""Please choose from the following by their number:
+        1: Add New Product
+        2: Add Stock
+        3: Remove Stock
+        4: Change Price
+        5: Find Product
+        6: Display inventory
+        7: Display inventory value
+        8: Remove product
+        9: Finish and Exit
+        Your decision: 
+        """)
+
+        try:
+            decision = int(decision)
+        except ValueError:
+            print("Invalid Input, please select the number value.")
+
+        if decision in range(1, 10):
+            break
+        else:
+            print("Invalid action, please choose from the menu provided.")
+
+    if decision == 1:
+        addPName = input("New Product Name: ")
+        addPSku = input("New Product's SKU: ")
+        addPPrice = input("New Product's Price: ")
+        addPQuantity = input("New Product's Quantity: ")
+
+        products[addPSku] = Product(addPName, addPSku, addPPrice, addPQuantity)
+
+        print("New Product Added:\n{}".format(products[addPSku]))
+
+    if decision == 2:
+        addStockTo = input("Product SKU: ")
+        amountToAdd = int(input("Add quantity: "))
+        products[addStockTo].add_stock(amountToAdd)
+
+    if decision == 3:
+        removeStockFrom = input("Product SKU: ")
+        amountToRemove = int(input("Remove Quantity: "))
+        products[removeStockFrom].remove_stock(amountToRemove)
+
+if __name__ == '__main__':
+    main()
--- Day 12/product.py
+++ Day 12/product.py
@@ -4,3 +4,23 @@ class Product:
         self.sku = sku #PK | Must be unique
         self.price = price
         self.quantity = quantity
+
+    def __str__(self):
+          return "Name: {}, SKU: {}, Price: ${}, Quantity: {}".format(
+                self.name,
+                self.sku,
+                self.price,
+                self.quantity
+          )
+
+    def add_stock(self, stock):
+            self.quantity += int(stock)
+            print("Total Stock for {} is now {}.".format(
+                self.name, self.quantity
+            ))
+
+    def remove_stock(self, amount):
+            self.quantity -= int(amount)
+            print("Total Stock for {} is now {}.".format(
+                self.name, self.quantity
+            ))
