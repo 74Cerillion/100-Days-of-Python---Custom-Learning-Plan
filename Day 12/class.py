26657ea152105839f6603984fd35f27f40ebd3c5
74Cerillion <jonhotchner@gmail.com>
2026-08-28 16:43:13 -0400

    Refactor day 12 product class

--- Day 12/class.py
+++
@@ -1,6 +0,0 @@
-class Product:
-    def __init__(self, name, sku, price, quantity):
-        self.name = name
-        self.sku = sku #PK | Must be unique
-        self.price = price
-        self.quantity = quantity
