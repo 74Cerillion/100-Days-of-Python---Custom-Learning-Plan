79dac02e43f6e4d28406b86b6fb3052618f9d447
74Cerillion <jonhotchner@gmail.com>
2026-09-01 20:55:13 -0400

    Day 15 code

---
+++ Day 15/main.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Switch:
+    hostname: str = field(default="localhost")
+    port: int = field(default=8080)
+    maxConnections: int = field(default=10)
+
+    def __post_init__(self):
+        if not self.hostname:
+            raise ValueError("Invalid Input, Expected truthy str.")
+        if not (0 < self.port < 65535):
+            raise ValueError("Invalid Port, Expected digit between 0-65535.")
+        if not (0 < self.maxConnections):
+            raise ValueError("Invalid input, Expected positive int")
+
+    def change_hostname(self, new_hostname: str):
+        if not new_hostname:
+            raise ValueError("Invalid Input, Expected truthy str.")
+        self.hostname = new_hostname
+
+normalConstruction = Switch('s1', 443, 3)
+defaultConstruction = Switch()
+
+try:
+    rejectedConstruction = Switch('', 70000, -1)
+except ValueError as e:
+    print(f"Rejected construction: {e}")
+
+print(normalConstruction == defaultConstruction)
