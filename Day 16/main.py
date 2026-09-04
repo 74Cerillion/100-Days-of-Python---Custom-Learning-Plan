ec1a89e937704cb6092be6d9aab3e20544549709
74Cerillion <jonhotchner@gmail.com>
2026-09-04 18:00:40 -0400

    Add classes

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,2 +1,30 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import strEnum
+
+class TicketStatus(strEnum):
+    pending = 'Pending'
+    in_progress = 'In Progress'
+    resolved = 'Resolved'
+    closed = 'Closed'
+
+@dataclass
+class Ticket:
+    id: int
+    title: str
+    status: TicketStatus = TicketStatus.pending
+
+    def __post_init__(self):
+        if not isinstance(self.status, TicketStatus):
+            raise ValueError(
+                f"Invalid status: {self.status}. Must be a TicketStatus."
+                )
+
+    def advance(self):
+        if self.status == TicketStatus.pending:
+            self.status = TicketStatus.in_progress
+        elif self.status == TicketStatus.in_progress:
+            self.status = TicketStatus.resolved
+        elif self.status == TicketStatus.resolved:
+            self.status = TicketStatus.closed
+        else:
+            raise ValueError("Cannot advance a closed ticket.")
