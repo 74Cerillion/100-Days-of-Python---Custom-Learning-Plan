2498b94788f0d3282a6c2aee3ed7b54019ccd1c0
74Cerillion <jonhotchner@gmail.com>
2026-09-13 17:13:24 -0400

    add faux-events in main.py

---
+++ Day 24/main.py
@@ -0,0 +1,8 @@
+events = [
+    {"host": "web-01", "severity": 3, "events": 17},
+    {"host": "db-02", "severity": 5, "events": 4},
+    {"host": "auth-01", "severity": 4, "events": 29},
+    {"host": "web-03", "severity": 2, "events": 41},
+    {"host": "db-01", "severity": 5, "events": 12},
+    {"host": "auth-02", "severity": 1, "events": 8},
+]
