031bbb0f39c8948004a5d05cb04fc9fdfb3cc7c1
74Cerillion <jonhotchner@gmail.com>
2026-09-14 12:15:11 -0400

    finish day 24

--- Day 24/main.py
+++ Day 24/main.py
@@ -6,3 +6,29 @@ events = [
     {"host": "db-01", "severity": 5, "events": 12},
     {"host": "auth-02", "severity": 1, "events": 8},
 ]
+
+#1
+req1 = sorted(events, key=lambda event: event['host'])
+print(req1)
+
+#2
+req2 = sorted(events, key=lambda event: event['severity'], reverse=True)
+print(req2)
+
+#3
+req3 = max(events, key=lambda event: event['events'])
+print(req3)
+
+#4
+req4 = sorted(
+    events, key=lambda event: (-event['severity'], -event['events'])
+)
+
+#Part 2
+def getSeverity(event):
+    return event["severity"]
+print(sorted(events, key=getSeverity))
+
+print(sorted(events, 
+             key=lambda event: event["severity"]
+             ))
