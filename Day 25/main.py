d5eb9aab1d948f3bfe97c8ee08fd74ddf2c589fe
74Cerillion <jonhotchner@gmail.com>
2026-09-15 18:30:18 -0400

    complete day 25

--- Day 25/main.py
+++ Day 25/main.py
@@ -3,3 +3,21 @@ servers = ["web-01", "db-01", "auth-01", "backup-01"]
 cpu_usage = [42, 91, 67, 28]
 
 services_running = [True, True, False, True]
+
+report = list()
+
+for server, cpu, service in zip(servers, cpu_usage, services_running):
+    report.append({
+        "server": server,
+        "cpu": cpu,
+        "service": service
+    })
+print(report)
+
+for ndx, i in enumerate(report, start=1):
+    print(ndx, i)
+
+if any(i['cpu'] > 85 for i in report):
+    print(True)
+if not all(i['service'] == True for i in report):
+    print(False)
